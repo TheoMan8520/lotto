2dd41de5720f2c982bd26c77dfc567f2fdf5abe8
TheoMan8520 <porramat1618@gmail.com>
2024-11-30 16:23:06 +0700

    initial user interface
    need input validation

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name='lotto'
 urlpatterns = [
     path("", views.MainView.as_view(), name="main"),
+    path("<int:mode>", views.MainView.as_view(), name="main_mode"),
 ]
--- app/views.py
+++ app/views.py
@@ -4,6 +4,32 @@ from django.views import View
 
 class MainView(View):
     template_name = 'main.html'
-    def get(self, request):
-        ctx = {}
+    def get(self, request, mode = 2):
+        result = ""
+        sixth = request.GET.get("sixth")
+        fifth = request.GET.get("fifth")
+        fourth = request.GET.get("fourth")
+        third = request.GET.get("third")
+        second = request.GET.get("second")
+        first = request.GET.get("first")
+        if(mode == 2 and sixth and fifth):
+            result="2ja"
+            # use query statement to trigger programable object
+        elif(mode == 3 and sixth and fifth and fourth):
+            result="3ja"
+            # use query statement to trigger programable object
+        elif(mode == 6 and sixth and fifth and fourth and third and second and first):
+            result="6ja"
+            # use query statement to trigger programable object
+        ctx = {
+            "mode": mode,
+            "result": result,
+            "sixth": sixth,
+            "fifth": fifth,
+            "fourth": fourth,
+            "third": third,
+            "second": second,
+            "first": first
+        }
         return render(request, self.template_name, ctx)
+
